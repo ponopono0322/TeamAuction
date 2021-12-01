1b32e4da2dbb5e6246f96126bfc468ed6f3e7e16
Jogyuin <94088237+Jogyuin@users.noreply.github.com>
2021-12-01 16:17:09 +0900

    경매장 리스트뷰 구현중
    리스트 뷰 틀 까지만 만들었어요

---
+++ app/src/main/java/com/example/teamauction/SellingItemScreen.java
@@ -0,0 +1,14 @@
+package com.example.teamauction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellingItemScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_item_selling);
+    }
+}
