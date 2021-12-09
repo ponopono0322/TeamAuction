d30c750106f44fb02e7b248d319414e158db080c
Jogyuin <94088237+Jogyuin@users.noreply.github.com>
2021-12-09 19:53:37 +0900

    BuyingScreen 추가
    AuctionScreen의 listview가 실질적으로 사용되게 하고 누르면 그 아이템에 대한 값을 저장, BuyingScreen이 실질적으로 작동되게 함

--- app/src/main/java/com/example/teamauction/AuctionScreen.java
+++ app/src/main/java/com/example/teamauction/AuctionScreen.java
@@ -23,7 +23,7 @@ public class AuctionScreen extends AppCompatActivity {
     private Button moveSellingButton;
     private Button moveMyitemButton;
 
-//경매장 리스트 만드는중
+    //경매장 리스트 만드는중
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +49,23 @@ public class AuctionScreen extends AppCompatActivity {
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher_background),
                 "Ind", "Assignment Ind Black 36dp") ;
 
+        //리스트 뷰에 있는 아이템 터치시 BuyingScreen popup창 띄우기
+        Button buyingbutton = findViewById(R.id.buyButton);
+        buyingbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int pos = listview.getCheckedItemPosition();
+                if (pos > -1) {
+                    ListViewItem item = (ListViewItem) adapter.getItem(pos);
+                    String ItemCode = item.getText();
+                    Intent intent = new Intent(AuctionScreen.this, BuyingScreen.class);
+                    intent.putExtra("data", ItemCode);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+
          //판매중, 내 아이템 이동버튼 구현
         moveSellingButton = (Button)findViewById(R.id.moveSellingButton); // 판매중인 아이템으로 이동버튼
         moveMyitemButton = (Button)findViewById(R.id.moveMyitemButton); // 내 아이템으로 이동버튼
@@ -60,7 +77,6 @@ public class AuctionScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         moveMyitemButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
---
+++ app/src/main/java/com/example/teamauction/BuyingScreen.java
@@ -0,0 +1,61 @@
+package com.example.teamauction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BuyingScreen extends AppCompatActivity {
+    EditText editTextcost, editTextquantity;
+    Button yes_btn, no_btn;
+    int request_num_yes, request_num_no;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //타이틀바 없애기
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.screen_buying);
+
+        //UI 객체생성
+        yes_btn = findViewById(R.id.check_yes);
+        no_btn = findViewById(R.id.check_no);
+
+        yes_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editTextcost = (EditText) findViewById(R.id.costBox);// 텍스트 읽어오기
+                editTextquantity = (EditText) findViewById(R.id.quantityBox);
+            }
+        });
+
+        no_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //바깥레이어 클릭시 안닫히게
+        if(event.getAction()== MotionEvent.ACTION_OUTSIDE){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //안드로이드 백버튼 막기
+        return;
+    }
+}
+
