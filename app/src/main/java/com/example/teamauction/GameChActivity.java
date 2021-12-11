967e4f745a3197ba29fbc1d500d36ee7bb9979c7
Kimminhyeo <94088147+Kimminhyeo@users.noreply.github.com>
2021-12-11 23:04:32 +0900

    Update GameChActivity.java

--- app/src/main/java/com/example/teamauction/GameChActivity.java
+++ app/src/main/java/com/example/teamauction/GameChActivity.java
@@ -1,8 +1,5 @@
 package com.example.teamauction;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +7,17 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class GameChActivity extends AppCompatActivity {
     private GameAccountInfo got_data;
@@ -38,6 +46,35 @@ public class GameChActivity extends AppCompatActivity {
         listview = (ListView) findViewById(R.id.listview2);
         listview.setAdapter(adapter);
 
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONArray games = jsonResponse.getJSONArray(got_data.getGameName());
+                    for (int i = 0; i < games.length(); i++) {
+                        JSONObject item = games.getJSONObject(i);
+                        String characterName = item.getString("gameNickname");
+                        //String gameNum = item.getString("GameNum");
+                        adapter.addItem(ContextCompat.getDrawable(GameChActivity.this,
+                                R.drawable.ic_baseline_account_box_24), characterName, "name");
+                    }
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) { // 접속 오류가 난 것이라면
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "no connection", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        Intent game_account_info = getIntent();
+        got_data = (GameAccountInfo) game_account_info.getSerializableExtra("game_account");
+        String purl = "http://ualsgur98.dothome.co.kr/CharacterList.php";
+        PHPRequest validateRequest = new PHPRequest( purl, got_data.getGameName(), got_data.getGamePublisherID(), got_data.getGamePublisherPW(), responseListener);
+        RequestQueue queue = Volley.newRequestQueue(GameChActivity.this);
+        queue.add(validateRequest);
+
+
+        /*
         // 첫 번째 아이템 추가.
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_baseline_account_box_24),
                 "Character_1","") ;
@@ -50,9 +87,8 @@ public class GameChActivity extends AppCompatActivity {
         // 네 번째 아이템 추가.
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_baseline_account_box_24),
                 "Character_4","") ;
+        */
 
-        Intent game_account_info = getIntent();
-        got_data = (GameAccountInfo) game_account_info.getSerializableExtra("game_account");
 
         Button selectCharacter = findViewById(R.id.select_game_character);
         selectCharacter.setOnClickListener(new View.OnClickListener() {
