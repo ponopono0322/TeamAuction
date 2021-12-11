9a9a0ccfc0928be926b0b5014898d6c4c75a68ee
MoonHyunjoon <94088220+MoonHyunjoon@users.noreply.github.com>
2021-12-12 01:28:44 +0900

    경매장 리스트 (미완성)
    경매장 리스트 (미완성)

--- app/src/main/java/com/example/teamauction/AuctionScreen.java
+++ app/src/main/java/com/example/teamauction/AuctionScreen.java
@@ -8,6 +8,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class AuctionScreen extends AppCompatActivity {
     private Button moveSellingButton;
@@ -29,15 +38,47 @@ public class AuctionScreen extends AppCompatActivity {
         listview = (ListView) findViewById(R.id.list);
         listview.setAdapter(adapter);
 
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONArray games = jsonResponse.getJSONArray("GameAuctionList");
+
+                    for (int i = 0; i < games.length(); i++) {
+
+                        JSONObject item = games.getJSONObject(i);
+                        String GameName = item.getString("GameName");
+                        String ItemName = item.getString("ItemName");
+                        String ItemQuantity = item.getString("ItemQuantity");
+                        String ItemPrice  = item.getString("ItemPrice");
+                        String GameID = item.getString("GameID");
+                        adapter.addItem(ContextCompat.getDrawable(AuctionScreen.this,
+                                R.drawable.ic_baseline_account_box_24),ItemName,ItemPrice);
+
+                    }
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) { // 접속 오류가 난 것이라면
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "no connection", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        String purl = "http://ualsgur98.dothome.co.kr/GameAuctionList.php";
+        PHPRequest validateRequest = new PHPRequest( purl, responseListener);
+        RequestQueue queue = Volley.newRequestQueue(AuctionScreen.this);
+        queue.add(validateRequest);
+
         // 첫 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher_background),
-               "Box", "Account Box Black 36dp") ;
+     //   adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher_background),
+            //   "Box", "Account Box Black 36dp") ;
         // 두 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher_background),
-                "Circle", "Account Circle Black 36dp") ;
+       // adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher_background),
+          //      "Circle", "Account Circle Black 36dp") ;
         // 세 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher_background),
-                "Ind", "Assignment Ind Black 36dp") ;
+        //adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher_background),
+           //     "Ind", "Assignment Ind Black 36dp") ;
 
         //리스트 뷰에 있는 아이템 터치시 BuyingScreen popup창 띄우기
         Button buyingbutton = findViewById(R.id.buyButton);
