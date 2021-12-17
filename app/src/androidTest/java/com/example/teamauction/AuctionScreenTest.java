9ffde6f583f7940e9e0a9d16bac9d193fe6b03a3
Jogyuin <94088237+Jogyuin@users.noreply.github.com>
2021-12-18 03:13:43 +0900

    capture
    capture

---
+++ app/src/androidTest/java/com/example/teamauction/AuctionScreenTest.java
@@ -0,0 +1,24 @@
+package com.example.teamauction;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import static org.hamcrest.Matchers.anything;
+import static org.junit.Assert.*;
+
+import android.content.Intent;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class AuctionScreenTest {
+
+}
